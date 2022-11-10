c1fcabceb9b4f45648f268450237f7e6820ca2fe
ncespedesc <41584216+ncespedesc@users.noreply.github.com>
2022-11-10 08:45:24 -0500

    Create package_to_install.R

---
+++ package_to_install_in_R/package_to_install.R
@@ -0,0 +1,29 @@
+
+#===========================================================#
+#       List of packages required by MHASpread              #
+#===========================================================#
+# the next lines require the package adn if is not pre-installed trigger the instalation request
+if (!require(shinydashboard)){install.packages("shinydashboard") };library(shinydashboard)
+if (!require(shinymanager)){install.packages("shinymanager") };library(shinymanager)
+if (!require(dashboardthemes)){install.packages("dashboardthemes") };library(dashboardthemes)
+if (!require(shiny)){install.packages("shiny") };library(shiny)
+if (!require(shinythemes)){install.packages("shinythemes") };library(shinythemes)
+if (!require(shinycssloaders)){install.packages("shinycssloaders") };library(shinycssloaders)
+if (!require(doParallel)){install.packages("doParallel") };library(doParallel)
+if (!require(magrittr)){install.packages("magrittr") };library(magrittr)
+if (!require(methods)){install.packages("methods") };library(methods)
+if (!require(Matrix)){install.packages("Matrix") };library(Matrix)
+if (!require(mc2d)){install.packages("mc2d") };library(mc2d)
+if (!require(snow)){install.packages("snow") };library(snow)
+if (!require(lubridate)){install.packages("lubridate") };library(lubridate)
+if (!require(DBI)){install.packages("DBI") };library(DBI)
+if (!require(scales)){install.packages("scales") };library(scales)
+if (!require(EpiContactTrace)){install.packages("EpiContactTrace") };library(EpiContactTrace)
+if (!require(sampler)){install.packages("sampler") };library(sampler)
+if (!require(sp)){install.packages("sp") };library(sp)
+if (!require(rgeos)){install.packages("rgeos") };library(rgeos)
+if (!require(spatialEco)){install.packages("spatialEco") };library(spatialEco)
+if (!require(parallel)){install.packages("parallel") };library(parallel)
+if (!require(ggsci)){install.packages("ggsci") };library(ggsci)
+if (!require(tidyverse)){install.packages("tidyverse") };library(tidyverse)
+if (!require(leaflet)){install.packages("leaflet") };library(leaflet)
