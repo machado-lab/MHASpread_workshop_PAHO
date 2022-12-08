e9df3b9cd2e3544ac9642f0e6368f7cfcaa0b8c2
ncespedesc <41584216+ncespedesc@users.noreply.github.com>
2022-12-08 08:36:51 -0500

    Update install_MHASpread.R

--- package_to_install_in_R/install_MHASpread.R
+++ package_to_install_in_R/install_MHASpread.R
@@ -2,7 +2,7 @@
 if (!require(remotes)){install.packages("remotes") };library(remotes)
 library(remotes)
 remotes::install_github(repo='machado-lab/MHASpread',
-                        auth_token = "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", force=T)
+                        auth_token = "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", force=T)
 #check
 require(MHASpread)
 if (!require(MHASpread)==F){hello()}
