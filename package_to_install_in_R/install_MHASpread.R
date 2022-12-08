f7aec6f5c67288b0f990eb4bb788162e246c1c7f
Gustavo Machado <42280272+gustavo-etal@users.noreply.github.com>
2022-12-08 10:00:46 -0500

    Update install_MHASpread.R

--- package_to_install_in_R/install_MHASpread.R
+++ package_to_install_in_R/install_MHASpread.R
@@ -2,7 +2,7 @@
 if (!require(remotes)){install.packages("remotes") };library(remotes)
 library(remotes)
 remotes::install_github(repo='machado-lab/MHASpread',
-                        auth_token = "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", force=T)
+                        auth_token = "", force=T)
 #check
 require(MHASpread)
 if (!require(MHASpread)==F){hello()}
