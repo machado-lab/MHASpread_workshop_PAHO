9a8fbfc3302d9551d98c263288e2d0382cfa0bb7
ncespedesc <41584216+ncespedesc@users.noreply.github.com>
2023-05-05 12:11:17 -0400

    Update package_to_install.R

--- package_to_install_in_R/package_to_install.R
+++ package_to_install_in_R/package_to_install.R
@@ -31,5 +31,6 @@ if (!require(leaflet.extras)){install.packages("leaflet.extras") };library(leafl
 if (!require(reshape2)){install.packages("reshape2") };library(reshape2) 
 if (!require(mapview)){install.packages("mapview") };library(mapview) 
 if (!require(taskscheduleR)){install.packages("taskscheduleR") };library(taskscheduleR) 
+if (!require(data.table)){install.packages("data.table") };library(data.table) 
 
 
