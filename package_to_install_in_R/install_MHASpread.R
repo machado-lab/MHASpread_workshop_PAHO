1a3847f750c1b0be1678f9229519da40afbc3608
ncespedesc <41584216+ncespedesc@users.noreply.github.com>
2023-05-23 10:47:20 -0400

    Update install_MHASpread.R

--- package_to_install_in_R/install_MHASpread.R
+++ package_to_install_in_R/install_MHASpread.R
@@ -1,5 +1,8 @@
 # Install R package 
 if (!require(remotes)){install.packages("remotes") };library(remotes)
+if (webshot::is_phantomjs_installed()==F) {  # additional software to screen shot
+  webshot::install_phantomjs()
+}
 library(remotes)
 remotes::install_github(repo='machado-lab/MHASpread',
                         auth_token = "replace_token_here",
