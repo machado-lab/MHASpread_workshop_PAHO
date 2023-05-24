9896a41c23d276b2717496fde9d216a86c3a5b50
ncespedesc <41584216+ncespedesc@users.noreply.github.com>
2023-05-24 14:49:26 -0400

    Update scenario_4.R

--- Control_action_scenarios/scenario_4/scenario_4.R
+++ Control_action_scenarios/scenario_4/scenario_4.R
@@ -1,34 +1,17 @@
-#=========================================================#
-#       Control actions disease workshop scenario #1      #
-#=========================================================#
-#load the package 
 library(MHASpread)
-#==========================================================================#
-#                                                                          #
-# Initial scenarios without control ---------------------------------------
-#                                                                          #
-#==========================================================================#
 
-# Select the farm or farms to be infected
-population <- MHASpread::population                     #   Get the population data example
+population <- MHASpread::population                                                # Get the population data example
 population$I_bov_pop[population$node== 196734] <- 40    #   Infected 40 bovine in the farm id = 196734
-# select the in and out farm dynamics (movements, births, deaths etc.. )
 events <- MHASpread::events
 
-#==========================================================================#
-#                                                                          #
-# Run the initial condition for  the model ---------------------------------
-#                                                                          #
-#==========================================================================#
 # run the 5 stochastic simulation
-##  Note: run the FMD model without any control action
-model_output <- stochastic_SEIR(number_of_simulation = 1,
-                                number_of_threads = 1, #parallel::detectCores()-1,
+model_output <- stochastic_SEIR(number_of_simulation = 10,
+                                number_of_threads = 5, #parallel::detectCores()-1,
                                 population = population,                                                 #  Population database
                                 events = events,                                                         #  Events database
                                 simulation_name = "scenario_1_init",                                     #  Simulation tag name
                                 days_of_simulation = 7,                                                 #  Population database
-                                initial_day_simulation=1,                                                #  Initial day of simulation
+                                initial_day_simulation=5,                                                #  Initial day of simulation
                                 max_distance_in_km= 40,                                                  #  Max distance kernel by local disease spread
                                 num_threads=1,                                                           #  Number of CPU to parallel tasks; set 1 to not overload your computer
                                 a = 0.012,                                                               #  To set kernel curve max infection rate (S*I)/N when animals are in the same area
@@ -49,12 +32,6 @@ model_output <- stochastic_SEIR(number_of_simulation = 1,
                                 lambda1_SR=c(min = 4, mode = 5, max = 14) ,                              #  Rate from exposed (E) to infectious (I) in small ruminants
                                 lambda2_SR=c(min = 6, mode = 15, max = 20))
 
-
-#==========================================================================#
-#                                                                          #
-# Explore the initial scenario (Non control actions) -----------------------
-#                                                                          #
-#==========================================================================#
 #=========================================================#
 ##  Initial spread epidemic curves of farms   ----
 #=========================================================#
@@ -94,7 +71,7 @@ mapview::mapshot(farms_location, file = "initial_outbreak_farms_location.png")  
 ##               Control zones areas    ----
 #=========================================================#
 # so if we establish control areas zones from a specific simulation == 1 looks like:
-detected_farms.id <- MHASpread::id_of_infectious_farms(model_output$populationdb,
+detected_farms.id <- MHASpread::id_of_infectious_farms(model_output[[1]]$populationdb,
                                                        only_infected_comp = F)
 
 zones_arond_inft_farms <- assign_control_zones(population = population,                      # Population database
@@ -120,18 +97,18 @@ control_model <- control_actions(
   population_data = MHASpread::population,   # Naive population  before the simulations
   events = MHASpread::events,                # Initial Scheduled movements
   break_sim_if = 50,                         # Breaks the simulation if there ar more than n infectious farms
-  
+
   # INITIAL CONDITION OF THE CONTROL ACTIONS
-  days_of_control_action = 20,               # Number of days to be working on control actions
+  days_of_control_action = 10,               # Number of days to be working on control actions
   detectection_rate = 50,                    # Detection rate per day (in percentage % (0%-100%))
   only_infected_comp = T,                    # If True will detect only animal in the infectious compartment
-  
+
   # CONTROL ZONES AREAS SETUP
   freq_updt_cntrl_zns = 7,                   # How often the control zones will be update i.e. 1, 7, 15 days
   infected_size_cz = 3,                      # Ratio size in Km of the infected zone
   buffer_size_cz = 7,                        # Ratio size in Km of the buffer zone
   surveillance_size_cz = 15,                 # Ratio size in Km of the surveillance zone
-  
+
   # ANIMAL MOVEMENTS STANDSTILL SETUP
   ban_length = 30,                           #  30 days of movements ban
   infected_zone_mov = T,                     #  Animal ban will be applied to infected zone
@@ -139,16 +116,16 @@ control_model <- control_actions(
   surveillance_zone_mov = T,                 #  Animal ban will be applied to surveillance zone
   direct_contacts_mov = T,                   #  Ban farm outside of control zones with contact with positive farms
   traceback_length_mov = 1,                  #  Traceback in-going animals movements of infected farms
-  
+
   # DEPOPULATION SETUP
-  limit_per_day_farms_dep = 4,               #  Farm will be depopulated by day
+  limit_per_day_farms_dep = 8,               #  Farm will be depopulated by day
   infected_zone_dep = T,                     #  Depopulation will be applied to infected zone
   only_depop_infect_farms = T,               #  If False stamping out all farms in the infected zone
-  
+
   # VACCINATION SETUP
   days_to_get_inmunity = 15,                 # How many days to be considered 100% immune
-  limit_per_day_farms = 25,                  # Maximum number of farms to be vaccinated in BUFFER area
-  limit_per_day_farms_infct = 25,            # Maximum number of farms to be vaccinated in INFECTED area
+  limit_per_day_farms = 20,                  # Maximum number of farms to be vaccinated in BUFFER area
+  limit_per_day_farms_infct = 30,            # Maximum number of farms to be vaccinated in INFECTED area
   vacc_eff = 0.7,                            # Numeric value between 0 and 1 indicating the efficacy of the vaccine
   dt = 1/15,                                 # Rate of conversion to SEIR -> V compartment i.e 1/15
   vacc_swine = T,                            # If true vaccine swine
@@ -159,42 +136,36 @@ control_model <- control_actions(
   vacc_infectious_farms =   T,               # If true infectious farms will be vaccinated
   vacc_delay = 5)                            # How many days until start the vaccination
 
-
-
-#==========================================================================#
-#                                                                          #
-# Plot results of control action modelling ---------------------------------
-#                                                                          #
-#==========================================================================#
 #==========================================#
 #            Epidemic distribution     ----
 #==========================================#
 # Lets see the results of the control actions when considering all farms types
 plot_epi_curve_mean_and_cntrl_act(model_inital = model_output,
-                                  model_control = control_output,
-                                  control_action_start_day = 11,
+                                  model_control = control_model,
+                                  control_action_start_day = 5,
                                   plot_only_total_farms = T)
 
 # Lets see the results of the control actions by each farms types
 plot_epi_curve_mean_and_cntrl_act(model_inital = model_output,
-                                  model_control = control_output,
-                                  control_action_start_day = 11,
+                                  model_control = control_model,
+                                  control_action_start_day = 5,
                                   plot_only_total_farms = F)
 
 #==========================================#
 #    Depopulated farms distribution    ----
 #==========================================#
 # Lets see the results of the depopulated farms over all simulation
-plot_depopulation(control_output = control_output, level_plot = "farms")
+plot_depopulation(control_output = control_model, level_plot = "farms")
 
 # Lets see the results of the depopulated animals over all simulation
-plot_depopulation(control_output = control_output, level_plot = "animals")
+plot_depopulation(control_output = control_model, level_plot = "animals")
 
 #==========================================#
 #    vaccinated farms distribution    ----
 #==========================================#
 # Lets see the results of the vaccinated farms over all simulation
-plot_vaccination(control_output = control_output, population = population, level_plot = "farms")
+plot_vaccination(control_output = control_model, population = population, level_plot = "farms")
 
 # Lets see the results of the vaccinated animals over all simulation
-plot_vaccination(control_output = control_output, population = population, level_plot = "animals")
+plot_vaccination(control_output = control_model, population = population, level_plot = "animals")
+
