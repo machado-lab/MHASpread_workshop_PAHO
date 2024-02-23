544248d8fb743dc7b2b4fe70236e9b6b462866d8
ncespedesc <41584216+ncespedesc@users.noreply.github.com>
2024-02-23 10:57:55 -0500

    Update package_to_install.R

--- package_to_install_in_R/package_to_install.R
+++ package_to_install_in_R/package_to_install.R
@@ -1,43 +1,31 @@
 #===========================================================#
-# set up all the necessary package for the model
+# set up all the necessary packages for the model
 #===========================================================#
-# the next lines require the package and if is not pre-installed trigger the installation request
+# The next lines require the package and if it is not pre-installed trigger the installation request
 
-if (!require(doParallel)){install.packages("doParallel") };library(doParallel)
-if (!require(magrittr)){install.packages("magrittr") };library(magrittr)
-if (!require(methods)){install.packages("methods") };library(methods)
-if (!require(Matrix)){install.packages("Matrix") };library(Matrix)
-if (!require(mc2d)){install.packages("mc2d") };library(mc2d)
-if (!require(snow)){install.packages("snow") };library(snow)
-if (!require(lubridate)){install.packages("lubridate") };library(lubridate)
-if (!require(DBI)){install.packages("DBI") };library(DBI)
-if (!require(scales)){install.packages("scales") };library(scales)
-if (!require(sampler)){install.packages("sampler") };library(sampler)
-if (!require(sp)){install.packages("sp") };library(sp)
-if (!require(rgeos)){install.packages("rgeos") };library(rgeos)
+if (!require(tidyverse)){install.packages("tidyverse") };library(tidyverse)
 if (!require(spatialEco)){install.packages("spatialEco") };library(spatialEco)
 if (!require(parallel)){install.packages("parallel") };library(parallel)
 if (!require(ggsci)){install.packages("ggsci") };library(ggsci)
-if (!require(tidyverse)){install.packages("tidyverse") };library(tidyverse)
 if (!require(leaflet)){install.packages("leaflet") };library(leaflet)
-if (!require(crayon)){install.packages("crayon") };library(crayon)
 if (!require(lwgeom)){install.packages("lwgeom") };library(lwgeom)
-if (!require(sf)){install.packages("sf") };library(sf)
-if (!require(cowsay)){install.packages("cowsay") };library(cowsay)
-if (!require(fastmatch)){install.packages("fastmatch") };library(fastmatch)
-if (!require(leaflet.extras)){install.packages("leaflet.extras") };library(leaflet.extras)
 if (!require(reshape2)){install.packages("reshape2") };library(reshape2)
 if (!require(mapview)){install.packages("mapview") };library(mapview)
-if (!require(data.table)){install.packages("data.table") };library(data.table)
-if (!require(remotes)){install.packages("remotes") };library(remotes)
 if (!require(devtools)){install.packages("devtools") };library(devtools)
-
-
-# add py 3 with python libraries
+if (!require(shiny)){install.packages("shiny") };library(shiny)
 if (!require(reticulate)){install.packages("reticulate") };library(reticulate)
+if (!require(webshot)){install.packages("webshot") };library(webshot)
+
+
+# add py 3 with Python libraries
 reticulate::install_miniconda()
 reticulate::py_install("pandas")
 reticulate::py_install("scipy")
-# if you still have issues try this
+reticulate::py_install("numpy")
+reticulate::py_install("pickle")
+reticulate::py_install("sys")
+
+
+# If you still have issues try this
 #conda_install("r-reticulate", "pandas")
 
